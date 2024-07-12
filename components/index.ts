8ec0598645f0a9b3ce5c164fe58a7f4b7eef67be
Ananthakumar Vithurshanan <lithuvithu1109@gmail.com>
2024-07-12 20:22:28 +0530

    adding webpage with some cars details

---
+++ components/index.ts
@@ -0,0 +1,24 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Searchbar from "./SearchBar";
+
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+
+
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    Searchbar,
+ 
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+
+}
